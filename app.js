65d6fb55aa6852fe11e8bf27cf93d4e2ee7293a6
hungvqnodejs <hungvqnodejs@gmail.com>
2022-01-15 23:14:28 +0700

    Lab 1.3 Gửi Response

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
-    console.log(req)
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><h1>Hello from my Node.js server!</h1></html>');
+    res.end();
 });
 server.listen(3000);
