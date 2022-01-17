94897b5f59ba4be50d8141f20f783b529e6e5935
hungvqnodejs <hungvqnodejs@gmail.com>
2022-01-18 02:06:59 +0700

    Lab 2.2: Cài đặt Middleware

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('in the Middleware');
+    next();
+});
+
+app.use((req, res, next) =>{
+    console.log('In another Middlewawre')
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
