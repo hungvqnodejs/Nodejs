ae2d5a8b3ed11ae30c796514ad58c95836a38af0
hungvqnodejs <hungvqnodejs@gmail.com>
2022-01-18 02:31:35 +0700

    Lab 2.4: Xử lý các Route khác nhau

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('in the Middleware');
+app.use('/', (req, res, next) =>{
+    console.log('This always run');
     next();
 });
 
-app.use((req, res, next) =>{
+app.use('/add-product', (req, res, next) =>{
+    console.log('In the Middlewawre');
+    res.write('<h1>The Add Product Page</h1>')
+});
+
+app.use('/', (req, res, next) =>{
     console.log('In another Middlewawre');
     res.write('<h1>Hello from Express.js</h1>')
 });
