d8c0ed1814e8105851c78d5f5ac29c8974ed2ae1
hungvqnodejs <hungvqnodejs@gmail.com>
2022-01-19 02:08:33 +0700

    Lab 2.12: Trả lại trang 404

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const res = require('express/lib/response');
 
 const app = express();
 
+const path = require('path')
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -14,7 +16,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
