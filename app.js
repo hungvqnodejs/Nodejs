ca930659421e129778eaea050eb313a9375e8782
hungvqnodejs <hungvqnodejs@gmail.com>
2022-01-22 02:36:33 +0700

    Lab 3.8: Thêm bố cục vào Handlebars

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const expressHbs = require('express-handlebars')
 
 const app = express();
 
-app.engine('hbs', expressHbs());
+app.engine('hbs', expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname: 'hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
@@ -20,7 +20,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+    res.status(404).render('404', {pageTitle: 'Page Not Found', productCSS: true });
 });
 
 app.listen(3000);
