b5ad1421835f45242e6a30788392478116d1d024
hungvqnodejs <hungvqnodejs@gmail.com>
2022-01-25 00:58:40 +0700

    Lab 4.4: Lưu trữ dữ liệu trong tệp thông qua model

--- models/product.js
+++ models/product.js
@@ -1,13 +1,31 @@
-const products = [];
+const fs = require('fs');
+const { connect } = require('http2');
+const path = require('path');
 
 module.exports = class Product {
     constructor(t){
         this.title = t
     }
     save(){
-        products.push(this)
+        const p = path.join(path.dirname(require.main.filename), 'data',' products,json');
+        fs.readFile(p, (err, fileContent) =>{
+            let products = [];
+            if (!err){
+                products = JSON.parse(fileContent);
+            }
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err) =>{
+                console.log(err);
+            });
+        });
     }
     static fetchAll(){
+        fs.readFile(p, (err, fileContent) => {
+            if (err){
+                return [];
+            }
+            return JSON.parse(fileContent);
+        })
         return products
     }
     
