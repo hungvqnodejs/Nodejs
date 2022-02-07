dd11dada06d7394ed4e5f1cf1fb99c069d4f3f57
hungvqnodejs <hungvqnodejs@gmail.com>
2022-02-08 01:57:14 +0700

    Lab 5.5: Thực hành Tìm nạp sản phẩm

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,33 +2,38 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render('shop/product-list', {
+        prods: rows,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
       path: '/products'
     });
   });
 };
 
-exports.getProduct = (req, res, next) => {
-    const prodId = req.params.productId;
-    Product.findById(prodId, product =>{
-        res.render('shop/product-detail', {product: product, pageTitle: product.title, path:'/products'});
-    });
-}
-
 exports.getIndex = (req, res, next) => {
   Product.fetchAll()
-         .then(([rows, fieldData]) => {
-          res.render('shop/index', {
-            prods: rows,
-            pageTitle: 'Shop',
-            path: '/'
-          });
-         })
-         .catch(err => console.log(err));
-  
+    .then(([rows, fieldData]) => {
+      res.render('shop/index', {
+        prods: rows,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
@@ -36,9 +41,11 @@ exports.getCart = (req, res, next) => {
     Product.fetchAll(products => {
       const cartProducts = [];
       for (product of products) {
-        const cartProductData = cart.products.find(prod => prod.id === product.id);
-        if (cart.products.find(prod => prod.id === product.id)) {
-          cartProducts.push({productData: product, qty: cartProductData.qty});
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
         }
       }
       res.render('shop/cart', {
@@ -46,26 +53,25 @@ exports.getCart = (req, res, next) => {
         pageTitle: 'Your Cart',
         products: cartProducts
       });
-    });    
+    });
   });
 };
 
-exports.postCart = (req, res, next) =>{
-    const prodId = req.body.productId;
-    Product.findById(prodId, (product) => {
-        Cart.addProduct(prodId, product.price)
-    })
-    res.redirect('/cart')
-}
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId, product => {
     Cart.deleteProduct(prodId, product.price);
     res.redirect('/cart');
-  })
-  
-}
+  });
+};
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
