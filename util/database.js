84a4aae0df921fbfd11dfe51d360d20dd188693b
hungvqnodejs <hungvqnodejs@gmail.com>
2022-02-12 03:06:45 +0700

    Lab 6.1: Cài đặt trình điều khiển MongoDB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,16 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('node-complete', 'root', '12345678', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+const mongoConnect = callback => {
+    MongoClient.connect('mongodb+srv://hung84:Password1@cluster0.mzvgb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+           .then(client => {
+               console.log('Connected!');
+               callback(client);
+           })
+           .catch(err => {
+               console.log(err);
+           }); 
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
+
