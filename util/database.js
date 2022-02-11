d3d88ba4d054140f506b225d2309ee91b1b077e5
hungvqnodejs <hungvqnodejs@gmail.com>
2022-02-12 03:28:11 +0700

    Lab 6.3: Tổng kết bài học kết nối cơ sở dữ liệu

--- util/database.js
+++ util/database.js
@@ -1,16 +1,30 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient
 
+let _db;
+
 const mongoConnect = callback => {
-    MongoClient.connect('mongodb+srv://hung84:Password1@cluster0.mzvgb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    MongoClient.connect('mongodb+srv://hung84:Password1@cluster0.mzvgb.mongodb.net/shop?retryWrites=true&w=majority')
            .then(client => {
                console.log('Connected!');
-               callback(client);
+               _db = client.db()
+               callback();
            })
            .catch(err => {
                console.log(err);
+               throw err;
            }); 
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
+
 
