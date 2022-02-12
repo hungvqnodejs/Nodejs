b72e19d7492c143cf35ebc2067382ab0ffd6832e
hungvqnodejs <hungvqnodejs@gmail.com>
2022-02-13 00:35:42 +0700

    Lab 6.7: Tìm nạp một sản phẩm duy nhất

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  Product.fetchAll()
     .then(products => {
       res.render('shop/product-list', {
         prods: products,
@@ -15,9 +15,8 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getProduct = (req, res, next) => {
-  Product.fetchAll()
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
+  Product.findById(prodId)
     .then(product => {
       res.render('shop/product-detail', {
         product: product,
@@ -81,7 +80,7 @@ exports.postCart = (req, res, next) => {
         newQuantity = oldQuantity + 1;
         return product;
       }
-      return Product.findByPk(prodId);
+      return Product.findById(prodId);
     })
     .then(product => {
       return fetchedCart.addProduct(product, {
--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
@@ -7,31 +8,48 @@ class Product {
     this.description = description;
     this.imageUrl = imageUrl;
   }
+
   save() {
-    const db = getDb()
-    return db.collection('products')
+    const db = getDb();
+    return db
+      .collection('products')
       .insertOne(this)
       .then(result => {
         console.log(result);
       })
       .catch(err => {
         console.log(err);
-      })
+      });
   }
 
   static fetchAll() {
-    const db = getDb()
+    const db = getDb();
     return db
       .collection('products')
       .find()
       .toArray()
       .then(products => {
         console.log(products);
-        return products
+        return products;
       })
       .catch(err => {
         console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then(product => {
+        console.log(product);
+        return product;
       })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
 
