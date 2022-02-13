04aae1da57392efc8f5927a06ab49efa5f48fc69
hungvqnodejs <hungvqnodejs@gmail.com>
2022-02-14 02:22:34 +0700

    Lab 6.14: Tạo người dùng mới

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/User');
 
 const app = express();
 
@@ -18,12 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
+  User.findById('6207ec692ed7d04db0d6e12f')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
   next()
 });
 
