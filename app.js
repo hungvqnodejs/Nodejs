7f20eadf5e4b2a4d25ef98ba4530741bc908c672
hungvqnodejs <hungvqnodejs@gmail.com>
2022-02-15 00:38:09 +0700

    Lab 6.17: Thêm chức năng "Thêm vào giỏ hàng"

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('620960c587f5b3d023e922da')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ class User {
   constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
-    this.cart = cart;
+    this.cart = cart; 
     this._id = id;
   }
 
@@ -17,14 +17,31 @@ class User {
   }
 
   addToCart(product) {
-    const updatedCart = { items: [{...product, quantity: 1 }] };
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity
+      });
+    }
+    const updatedCart = {
+      items: updatedCartItems
+    };
     const db = getDb();
     return db
-        .collection('user')
-        .updateOne(
-            { _id: new ObjectId(this, _id) },
-            { $set: {cart: updatedCart}}
-        );
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
