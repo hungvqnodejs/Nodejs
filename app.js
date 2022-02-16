3330f1302b01e07a72a60c416a98c778768c38e0
hungvqnodejs <hungvqnodejs@gmail.com>
2022-02-17 01:29:19 +0700

    Lab 7.3: Lưu dữ liệu thông qua Mongoose

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('620960c587f5b3d023e922da')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('620960c587f5b3d023e922da')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -33,7 +33,7 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect('mongodb+srv://hungvq:Pass1@cluster0.mzvgb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  .connect('mongodb+srv://hungvq:Pass1@cluster0.mzvgb.mongodb.net/shop?retryWrites=true&w=majority')
   .then(result => {
     app.listen(3000);
   })
