05c8a88ae4d51ff26abec8e835bdfe6eac4c2ce3
hungvqnodejs <hungvqnodejs@gmail.com>
2022-02-17 02:33:15 +0700

    Lab 7.9: Sử dụng quan hệ trong Mongoose

--- models/user.js
+++ models/user.js
@@ -14,7 +14,7 @@ const userSchema = new Schema({
     cart: {
         items: [
             {
-                productId: { type: Schema.Types.ObjectId, required: true },
+                productId: { type: Schema.Types.ObjectId, ref: 'product', required: true },
                 quantity: { type: Number, required: true }
             }
         ]
