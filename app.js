be60503f5739daada38b664203f8827b60caf085
hungvqnodejs <hungvqnodejs@gmail.com>
2022-03-02 03:14:47 +0700

    Lab 8.2: Thiết lập Cookie

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const authpRoutes = require('./routes/auth');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(authpRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
