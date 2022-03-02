ca4eb97172725fafb9a94a7c7c184c9a550f7fff
hungvqnodejs <hungvqnodejs@gmail.com>
2022-03-02 23:22:58 +0700

    Lab 8.8: Xóa Cookie

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb+srv://hungvq:Pass1@cluster0.mzvgb.mongodb.net/shop?retryWrites=true&w=majority'
+const MONGODB_URI =
+'mongodb+srv://hungvq:Pass1@cluster0.mzvgb.mongodb.net/shop?retryWrites=true&w=majority';
 
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
-  collection: 'session'
-})
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -26,16 +27,14 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store }))
-
-app.use((req, res, next) => {
-  User.findById('620d4d6983ad493113e70cc0')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -57,7 +56,7 @@ mongoose
         });
         user.save();
       }
-    }) 
+    });
     app.listen(3000);
   })
   .catch(err => {
