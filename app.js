52e73073042c6a9916b03d8f71d633d39561b3ba
hungvqnodejs <hungvqnodejs@gmail.com>
2022-03-02 22:47:23 +0700

    Lab 8.7: Sử dụng MongoDB để lưu trữ Session

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://hungvq:Pass1@cluster0.mzvgb.mongodb.net/shop?retryWrites=true&w=majority'
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'session'
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,7 +26,7 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false }))
+app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store }))
 
 app.use((req, res, next) => {
   User.findById('620d4d6983ad493113e70cc0')
@@ -37,7 +44,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect('mongodb+srv://hungvq:Pass1@cluster0.mzvgb.mongodb.net/shop?retryWrites=true&w=majority')
+  .connect(MONGODB_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
