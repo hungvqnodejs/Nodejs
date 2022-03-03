6d73f66bd7fae11de1f275b4bb2079ac11ed263d
hungvqnodejs <hungvqnodejs@gmail.com>
2022-03-04 01:05:43 +0700

    Lab 8.19: Sử dụng Middleware để bảo mật cho Router

--- routes/admin.js
+++ routes/admin.js
@@ -3,22 +3,23 @@ const path = require('path');
 const express = require('express');
 
 const adminController = require('../controllers/admin');
+const isdAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
+router.get('/add-product', isdAuth, adminController.getAddProduct);
 
 // /admin/products => GET
-router.get('/products', adminController.getProducts);
+router.get('/products', isdAuth, adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', adminController.postAddProduct);
+router.post('/add-product', isdAuth, adminController.postAddProduct);
 
-router.get('/edit-product/:productId', adminController.getEditProduct);
+router.get('/edit-product/:productId', isdAuth, adminController.getEditProduct);
 
-router.post('/edit-product', adminController.postEditProduct);
+router.post('/edit-product', isdAuth, adminController.postEditProduct);
 
-router.post('/delete-product', adminController.postDeleteProduct)
+router.post('/delete-product', isdAuth, adminController.postDeleteProduct)
 
 module.exports = router;
