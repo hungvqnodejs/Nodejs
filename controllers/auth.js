e1e66574fc8862b54e3590fc1433cfe0a6c70c49
hungvqnodejs <hungvqnodejs@gmail.com>
2022-03-04 00:04:42 +0700

    Lab 8.14: Triển khai Luồng xác thực

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,14 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
+
 exports.postLogin = (req, res, next) => {
   User.findById('620d4d6983ad493113e70cc0')
     .then(user => {
@@ -21,9 +29,34 @@ exports.postLogin = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        return res.redirect('/signup');
+      }
+      const user = new User({
+        email: email,
+        password: password,
+        cart: { items: [] }
+      });
+      return user.save()
+    })
+    .then(result => {
+      res.redirect('/login')
+    })
+    .catch(err => console.log(err));
+};
+
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
     console.log(err);
     res.redirect('/');
   });
 };
+
+
