a51470277f2d5c92556c5a892d4e2b1bff51ca18
hungvqnodejs <hungvqnodejs@gmail.com>
2022-03-07 01:12:28 +0700

    Lab 9.8: Thêm các lớp CSS có điều kiện

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,7 +45,8 @@ exports.getSignup = (req, res, next) => {
       email: '',
       password: '',
       confirmPassword: ''
-    }
+    },
+    validationErrors: []
   });
 };
 
@@ -105,7 +106,8 @@ exports.postSignup = (req, res, next) => {
         email: email,
         password: password,
         confirmPassword: req.body.confirmPassword
-      }
+      },
+      validationErrors: errors.array()
     });
   }
 
