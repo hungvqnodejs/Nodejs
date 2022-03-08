9ab2328e66b7725d441ee266cc5987ab6f1869ea
hungvqnodejs <hungvqnodejs@gmail.com>
2022-03-09 00:47:59 +0700

    Lab 10.6: Lưu trữ dữ liệu tệp trong cơ sở dữ liệu

--- controllers/admin.js
+++ controllers/admin.js
@@ -111,7 +111,7 @@ exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
+  const image = req.file;
   const updatedDesc = req.body.description;
 
   const errors = validationResult(req);
@@ -124,7 +124,9 @@ exports.postEditProduct = (req, res, next) => {
       hasError: true,
       product: {
         title: updatedTitle,
-        imageUrl: updatedImageUrl,
+        if (image) {
+          product.imageUrl = image.path;
+        },
         price: updatedPrice,
         description: updatedDesc,
         _id: prodId
@@ -142,6 +144,7 @@ exports.postEditProduct = (req, res, next) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
+
       product.imageUrl = updatedImageUrl;
       return product.save().then(result => {
         console.log('UPDATED PRODUCT!');
